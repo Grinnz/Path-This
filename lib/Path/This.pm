318a161312fb5c729e3f28ad58722c59d1edf230
Dan Book <grinnz@grinnz.com>
2019-10-28 11:55:19 -0400

    initial commit

---
+++ lib/Path/This.pm
@@ -0,0 +1,35 @@
+package Path::This;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+Path::This - Module abstract
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Report any issues on the public bugtracker.
+
+=head1 AUTHOR
+
+Dan Book <dbook@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2019 by Dan Book.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=head1 SEE ALSO
+
